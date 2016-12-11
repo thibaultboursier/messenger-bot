095724d2a773a4303e5186b22df90098b33324fa
thibaultboursier <contact@thibaultboursier.fr>
2016-12-11 22:48:22 +0100

    going on implementing bot

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(app.get('port'), function () {
 
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging;
-    console.log(event);
+    console.log(req);
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i];
         let sender = event.sender.id;
